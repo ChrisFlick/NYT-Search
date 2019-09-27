961d4f7061c57acff41399c1cf6495add9dc381a
Chris Flick <chrisflick@gmail.com>
2019-09-27 14:41:08 -0700

    getArticle now defaults if no startYear and endYear is given

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,18 @@
 // getArticles("", "20070101", "20081228");
 
 function getArticles (terms, startYear, endYear){
-    let beginDate = startYear + '0101'
-    let endDate = endYear + "1228"
+    if (startYear) {
+        let beginDate = startYear + '0101'
+    } else {
+        let beginDate = ""
+    }
+    
+    if (endYear) {
+        let endDate = endYear + "1228"
+    } else {
+        let endDate = "";
+    }
+    
     let queryURL = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${terms}&begin_date=${beginDate}&end_date=${endDate}&api-key=${APIKey}`;
 
     $.ajax({
