411e4097eb3bd048241a04efd57ba7c069654444
Chris Flick <chrisflick@gmail.com>
2019-09-28 15:36:39 -0700

    Now shows picture from article

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,30 +10,41 @@ $(document).ready( function() {
       for (let i = 0; i < resultNum; i++) {
         let article = response.response.docs[i]
 
-        let $row = $('<div>')
+        let $row1 = $('<div>')
+        let $row2 = $('<div>')
         let $col1 = $('<div>')
         let $col2 = $('<div>')
         let $img = $('<img>')
-        let $abstract = $('<p>')
-        let $link = $('<a>')
+        let $abstract = $('<a>')
         let $headline = $('<h3>')
+        let $break = $('<br>')
 
-        $link.attr('href', article.web_url)
-        $link.attr('target', '_blank');
-
+        $headline.append($break);
         $headline.text(article.headline.main)
+        $headline.append($break); // Ensure picture is on next line
+
+
+        $img.attr('src', 'https://www.nytimes.com/' + article.multimedia[0].url)
+        $img.css('height', "150px")
+
+        $row1.attr('class', 'row')
+        $row2.attr('class', 'row')
+        $col1.attr('class', 'col-md-6')
+        $col2.attr('class', 'col-md-6')
+
         $abstract.text(article.abstract)
+        $abstract.attr('href', article.web_url)
+        $abstract.attr('target', '_blank');
 
-        $row.append($link);
-
-        $link.append($headline);
-        $link.append($col1);
-        $link.append($col2);
+        $row1.append($headline)
+        $row2.append($col1);
+        $row2.append($col2);
 
         $col1.append($img);
         $col2.append($abstract);
 
-        $('#search-results').append($row)
+        $('#search-results').prepend($row2)
+        $('#search-results').prepend($row1)
       }
     });
   });
@@ -43,5 +54,6 @@ $(document).ready( function() {
     $('#result-number').val("1")
     $('#start-year').val("")
     $('#end-year').val("")
+    $('#search-results').html('')
   })
 });
